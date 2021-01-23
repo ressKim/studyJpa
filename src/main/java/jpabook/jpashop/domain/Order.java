f219cacdf8aa90759022409b14ed9ffae150f2c8
jun <sh21dlat@gmail.com>
2021-01-23 22:16:50 +0900

    entity study and add

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToMany
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+
+}
