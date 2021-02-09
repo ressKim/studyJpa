41cf29c3341649e5aa60015d4d6c6dc22f350d16
ressKim <sh21dlat@gmail.com>
2021-02-09 22:00:08 +0900

    lazy setting add , 연관관계 편의 메서드 추가 - set 할때 엮여있는것 같이 저장하기, cascade = CascadeType.ALL 로 설정함으로써 oneToMany 엮여있는걸 한번에 저장, 삭제 가능하도록 변경

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -32,6 +32,7 @@ public class Order {
 
     private LocalDateTime orderDate; //주문시간 @Enumerated(EnumType.STRING)
 
+    @Enumerated(EnumType.STRING)
     private OrderStatus status; //주문상태 [ORDER, CANCEL]
 
     //==연관관계 메서드==//
