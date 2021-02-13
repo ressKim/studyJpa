d5dbf0e06d23686c02d0451b8145f9bb22467801
ressKim <sh21dlat@gmail.com>
2021-02-13 20:35:11 +0900

    변경감지 와 병합(merge) study - 결론 : 병합대신 변경감지를 쓰자! set 말고 updateCount 식으로라도 만들어서 entity 에서 관리를 다 하자

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +20,15 @@ public class ItemService {
     public void saveItem(Item item) {
         itemRepository.save(item);
     }
+
+    @Transactional //이런식으로 하거나 요소가 많으면 dto 를 만들어서 하는것이 좋다.
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setPrice(price);
+        findItem.setName(name);
+        findItem.setStockQuantity(stockQuantity);
+    }
+
 
     public List<Item> findItems() {
         return itemRepository.findAll();
