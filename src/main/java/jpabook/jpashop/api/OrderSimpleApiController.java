f8f647d69a676a056efc05ac08ae7f95d1e03730
ressKim <sh21dlat@gmail.com>
2021-02-14 20:46:08 +0900

    주문 조회를 DTO 로 처리 , 그러나 여기도 쿼리가 N + 1 번 나가는 이슈가 있어서 이렇게 하고 fetch join 을 해야한다.

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,20 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * xToOne(ManyToOne, OneToOne)
@@ -30,8 +37,43 @@ public class OrderSimpleApiController {
             order.getMember().getName();//Lazy 강제 초기화
             order.getDelivery().getAddress();//Lazy 강제 초기화
         }
-
         return all;
     }
+
+    @GetMapping("/api/v2/sample-orders")
+    public List<SimpleOrderDto> ordersV2(){
+        //ORDER 2개
+        //N + 1 -> 1 + 화원 N + 배송 N
+
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        List<SimpleOrderDto> collect = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return collect;
+
+//        return orderRepository.findAllByString(new OrderSearch()).stream()
+//                .map(SimpleOrderDto::new)
+//                .collect(toList());
+
+    }
+
+    @Data
+    static class SimpleOrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); //LAZY 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); //LAZY 초기화
+        }
+    }
+
 
 }
